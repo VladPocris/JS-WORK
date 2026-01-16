6b91ed65336b76524c87ee80714f2e7e48655f45
Vlad Pocris <vpocris@gmail.com>
2026-01-16 17:46:26 +0000

    Sentence Maker

---
+++ SentenceMaker/script.js
@@ -0,0 +1,21 @@
+let adjective = "big";
+let noun = "dragon";
+let verb = "flying";
+let place = "castle";
+let adjective2 = "outstanding";
+let noun2 = "rabbits";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "small";
+noun = "bee";
+verb = "flying";
+place = "bee-hive";
+adjective2 = "outstanding";
+noun2 = "honey";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
