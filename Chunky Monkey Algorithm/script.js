e54ee93e3262929e7361f84898ea223828f79121
Vlad Pocris <vpocris@gmail.com>
2026-01-23 03:26:11 +0000

    Chunky Monkey Algorithm

---
+++ Chunky Monkey Algorithm/script.js
@@ -0,0 +1,11 @@
+function chunkArrayInGroups(arr, num) {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i += num) {
+    result.push(arr.slice(i, i + num));
+  }
+
+  return result;
+}
+
+console.log(chunkArrayInGroups([1,2,3,4,5], 2));
